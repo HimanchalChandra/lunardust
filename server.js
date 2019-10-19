5181dc34c808beb2910f1b5ed576262b7a1c2903
d-mckee <devon.l.mckee@gmail.com>
2019-10-19 14:50:26 -0700

    fixing index.html stylesheet link and uploading newest server.js

--- server.js
+++ server.js
@@ -1,6 +1,26 @@
 const express = require('express');
 const app = express();
+const broker = "mqtt://mqtt.eclipse.org";
+const topic = "esp2866";
+const mqtt = require('mqtt');
+const client = mqtt.connect(broker);
 
+
+
+client.on("connect", function() {
+	client.subscribe(topic, function(err){
+		if(!err){
+			console.log("Subscribed to '" + topic + "' with broker '" 
+						+ broker + "'.");
+		}
+	});
+});
+
+
+
+client.on("message", function (topic, message) {
+	console.log("Received: " + message.toString());
+});
 app
 	.use(express.static(__dirname + "/web"));
 
@@ -8,5 +28,5 @@ app
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-	console.log("Server listening on port ${PORT}...");
+	console.log("Server listening on port " + PORT + "...");
 });
